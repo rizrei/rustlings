c04aaf18a1565e7c7fc1b14d390158384ecfac58
Albert Gabitov <agabitov@evrone.com>
2026-06-07 18:40:46 +0300

    Implement `01_variables` module exercises and solutions
    This introduces the full set of Rustlings exercises for the `01_variables` module. It includes the initial exercise files for learners to solve and their corresponding completed solutions.

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,10 +1,5 @@
 fn main() {
-    // TODO: Change the line below to fix the compiler error.
-    let x;
+    let x = 42;
 
-    if x == 10 {
-        println!("x is ten!");
-    } else {
-        println!("x is not ten!");
-    }
+    if x == 10 {println!("x is ten!") } else { println!("x is not ten!")}
 }
